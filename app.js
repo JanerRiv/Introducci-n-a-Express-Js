9b9aeb5ec5b13b978b7522868d5975f54e0cb76f
JanerRiv <jr@gmail>
2026-07-31 14:05:17 -0500

    importacion y descarga de dotenv

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import {configDotenv} from "dotenv";
+
+configDotenv()
 
 const app = express();
-const port = 3000;
+const port = process.env.PUERTO || 3000;
 
 app.get("/", (_, res) => {
 res.send('Aprendicez ficha 3407186');
 });
 
 app.listen(port, () => {
-console.log( `Servidor en funcionamiento en el puerto: ${port}`);
+console.log( `Servidor en funcionamiento en el puerto: http://localhost:${port}`);
 });
 
